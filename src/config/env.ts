8228950a388859212e075db7e708ac02a9fbbf0b
lucianoleyria <lucholeyria@gmail.com>
2024-04-08 15:19:13 -0300

    Feature: Implement nats as transporter

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,5 @@
 export const EnvConfig = () => ({
     PORT: process.env.PORT,
     DATABASE_URL: process.env.DATABASE_URL,
+    NATS_SERVERS: process.env.NATS_SERVERS.split(','),
 });
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -17,22 +17,22 @@ export class ProductsController {
     return this.productsService.create(createProductDto);
   }
 
-  @MessagePattern({ cmd: 'find_all' })
+  @MessagePattern({ cmd: 'find_all_products' })
   findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
-  @MessagePattern({ cmd: 'find_one' })
+  @MessagePattern({ cmd: 'find_one_product' })
   findOne(@Payload() id: number) {
     return this.productsService.findOne(id);
   }
 
-  @MessagePattern({ cmd: 'update' })
+  @MessagePattern({ cmd: 'update_product' })
   update(@Payload() updateProductDto: UpdateProductDto) {
     return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
-  @MessagePattern({ cmd: 'delete' })
+  @MessagePattern({ cmd: 'delete_product' })
   remove(@Payload() id: number) {
     return this.productsService.remove(id);
   }
